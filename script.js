ce10253cffd67c6622c46afe65eb8ab41b07da72
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-20 17:53:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+//SGN your JS code here. If required.
+
+let time = document.getElementById("timer");
+let am = "AM";
+
+let currentdate = new Date();
+if(currentdate.getHours()>12){
+	am = "PM";
+}else {
+	am = "AM";
+}
+var datetime = currentdate.getMonth() 
++ "/" + currentdate.getDay() + "/" +  currentdate.getFullYear() + ", " 
++ currentdate.getHours() + ":" 
++ currentdate.getMinutes() + ":" + currentdate.getSeconds() + " " + am;
+time.innerHTML = datetime;
